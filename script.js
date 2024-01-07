3d877b0e18170aa4c8b29b79ac7b0532128c7f0f
kuderluka <luka.kuder@gmail.com>
2024-01-07 22:55:20 +0100

    Operations and system fetching
    Operations and the system can now be fetched

--- script.js
+++ script.js
@@ -1,15 +1,43 @@
-function appendToResult(value) {
+function calculateResult() {
+
+    //Do validation if the system and operations are selected
+    var system = document.getElementsByClassName('system')[0].textContent;
+    console.log(system);
+
+    var operation = document.getElementsByClassName('operation')[0].textContent;
+    console.log(operation);
+
+
+
+    // try {
+    //     document.getElementById('result').value = eval(document.getElementById('result').value);
+    // } catch (error) {
+    //     document.getElementById('result').value = 'Error';
+    // }
+}
+
+function appendToInput(value) {
     document.getElementById('result').value += value;
 }
 
-function clearResult() {
+function clearInput() {
     document.getElementById('result').value = '';
 }
 
-function calculateResult() {
-    try {
-        document.getElementById('result').value = eval(document.getElementById('result').value);
-    } catch (error) {
-        document.getElementById('result').value = 'Error';
-    }
+function setOperation(clickedButton) {
+    var buttons = document.querySelectorAll('.btn');
+    buttons.forEach(function(button) {
+      button.classList.remove('operation');
+    });
+
+    clickedButton.classList.add('operation');
+}
+
+function setSystem(clickedButton) {
+    var buttons = document.querySelectorAll('.btn');
+    buttons.forEach(function(button) {
+      button.classList.remove('system');
+    });
+
+    clickedButton.classList.add('system');
 }
